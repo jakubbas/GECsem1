64620f8f28cbbe00a8fbffab5cfc8cfb45ac7bfa
Jacob BAS <b014818k@student.staffs.ac.uk>
2020-10-08 01:16:47 +0100

    Added the area of rectangle project, for some reason doesn't work?

---
+++ Program6_area/Program6_area/Program6_area.cpp
@@ -0,0 +1,17 @@
+// Program6_area.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int width = 15;
+	int height = 25;
+
+	int area = (width * height);
+	std::cout << "The area of a " << width << " by " << height << " rectangle is " << area << endl;
+
+	return 0;
+
+}
+
